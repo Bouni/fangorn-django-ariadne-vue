717c34cf32fbc17d55d68c535e913a353e8b4f06
Bouni <bouni@owee.de>
2022-02-09 16:24:35 +0100

    fixed typo

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djose.urls.jwt')),
-    path('auth/', include('djose.urls')),
-    path('auth/', include('djose.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
